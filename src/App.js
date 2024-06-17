c13f682c84eb0fd507250252f87decb6d349c5b1
Christopher Aondokaa <Christopher.Aondokaa@kominiti.net>
2024-06-17 14:24:01 +0100

    my first task

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material"
 const App = () => {
   const location = useLocation()
   const navigate = useNavigate()
+
   // adding pagination
+
   const pages = [
     { path: "/", label: "1" },
     { path: "/video", label: "2" },
